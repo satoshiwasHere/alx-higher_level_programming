ff3a42451194a1b72236ef7cc8cd1a83c501e5a2
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-07 06:29:16 -0700

    More queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- Script lists all shows contained in hbtn_0d_tvshows
-SELECT show.`title`, gnr.`genre_id` FROM `tv_shows` AS show
-        INNER JOIN `tv_show_genres` AS gnr
-	ON show.`id` = gnr.`show_id'
- ORDER BY show.`title`, gnr.`genre_id`;
+SELECT sh.`title`, gn.`genre_id`
+  FROM `tv_shows` AS sh
+        INNER JOIN `tv_show_genres` AS gn
+	ON sh.`id` = gn.`show_id`
+ ORDER BY sh.`title`, gn.`genre_id`;
