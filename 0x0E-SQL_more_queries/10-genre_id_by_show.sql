bdbdc4b724f8a3e292a30d7587a003ebd7edda59
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-07 06:26:00 -0700

    More queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,3 @@
--- Import the database dump from hbtn_0d_tvshows to your MySQL server:
 -- Script lists all shows contained in hbtn_0d_tvshows
 SELECT show.`title`, gnr.`genre_id` FROM `tv_shows` AS show
         INNER JOIN `tv_show_genres` AS gnr
