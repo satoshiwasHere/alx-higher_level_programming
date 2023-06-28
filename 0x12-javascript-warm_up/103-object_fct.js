c84f8f102fdce6f7d9e3a3ac32d6116736fd1663
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-28 08:09:32 -0700

    Js_warm_up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
 
+// Updates this script by adding a new function incr that increments the integer value.
+
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
-  myObject.value += 1;
+  this.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
-console.log(myObject)
+console.log(myObject);
