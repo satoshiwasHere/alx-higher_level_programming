d3c17ac541533192e6636a5546700214f039b8a0
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-28 13:31:34 -0700

    js_objects_scopes

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ module.exports = class Square extends require('./5-square.js') {
     if (c === undefined) {
       this.print();
     } else {
-      for (let v = 0; v < this.height; v++) console.log(c.repeat(this.width));
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
