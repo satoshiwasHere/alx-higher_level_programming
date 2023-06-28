492bedbe576b90d40ca5c93af0bf508ab72fedd5
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-28 07:43:57 -0700

    Js_warm_up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 
 // script that prints 3 lines
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+
+console.log(myVar);
