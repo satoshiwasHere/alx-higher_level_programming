e2b35c0b922e0c38d6d60721540f31d05f745592
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-28 07:51:36 -0700

    Js_warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,8 +3,9 @@
 // script prints 3 lines: (like 1-multi_languages.js)
 // by using an array of string and a loop
 
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let z;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const string of strings) {
-  console.log(string);
+for (z = 0; z < 3; z++) {
+  console.log(myString[z]);
 }
