fdffe671b35aafcb1e368f745a0c37cbfe6acfdc
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-28 07:47:09 -0700

    Js_warm_up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,13 +3,9 @@
 // script prints a message depending of the number of arguments passed
 
 const argc = process.argv.length;
-if (argc > 3)
-{
-  console.log('Arguments found');
-} else if (argc === 3)
-{
-  console.log('Argument found');
-} else
-{
+
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+} else {
   console.log('No argument');
 }
