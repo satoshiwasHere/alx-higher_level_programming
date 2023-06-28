3e2ab61efe0d9b1672ab06b6d7b95be37261daab
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-28 13:34:25 -0700

    js_objects_scopes

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,12 +4,14 @@
  * A class Square that defines a square and inherits from Square of 5-square.js
  */ 
 
-module.exports = class Square extends require('./5-square.js') {
+const square = require('./5-square');
+class Square extends square {
   charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    if (!c) {
+      c = 'X';
     }
+    this.print(c);
   }
-};
+}
+
+module.exports = Square;
