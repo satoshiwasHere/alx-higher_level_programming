b5bedc2eed1f2bc21e51955e999a21452325f1f4
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-30 06:40:41 -0700

    python_network0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Script takes in a URL, sends a request to that URL
-# displays the size of the body of the response
-# size of display in bytes
-# curl is used
-
+# Script takes in a URL, sends a request to that URl
 curl -s "$1" | wc -c
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Script sends DELETE request to the URL passed as
-# the first argument and displays the body of the response
-# curl is utilized
-
+# Script sends DELETE request to the URL passed as the first argument
 curl -sX DELETE "$1"
