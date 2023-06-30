9536eb9acbee2cbc88a210bcd0e5462fdc8069ce
satoshiwasHere <skinyanjui100@gmail.com>
2023-06-30 15:23:14 -0700

    python-network1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,16 +4,17 @@ Takes in a URL and sends a request to the URL
 and displays the body of the response 'decoded in utf-8'
 """
 
-import urllib.request
 import sys
+import urllib.error
+import urllib.request
+
 
 if __name__ == "__main__":
-    try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode("utf-8"))
+    url = sys.argv[1]
 
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
     except urllib.error.HTTPError as er:
         print("Error code: {}".format(er.code))
-
-    except:
-        pass
